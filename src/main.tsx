2773d0bf9cc527fa8e67f8b2ebbd5d36f07178bc
momar <muhammad.omar1141@gmail.com>
2026-03-03 01:43:28 -0500

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider, theme } from "antd";
+import App from "./App.tsx";
+import "antd/dist/reset.css";
+
+function Root() {
+const [dark, setDark] = React.useState(false);
+
+React.useEffect(() => {
+  localStorage.setItem("darkMode", JSON.stringify(dark));
+}, [dark]);
+
+  const config = React.useMemo(
+    () => ({
+      algorithm: dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      token: {
+        colorPrimary: "#1677ff",
+      },
+    }),
+    [dark]
+  );
+
+  return (
+    <ConfigProvider theme={config}>
+      <App/>
+    </ConfigProvider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);

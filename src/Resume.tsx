6aaa47d38822d658b5a46dc925b6d23846de8fc8
momar <muhammad.omar1141@gmail.com>
2026-03-04 23:33:33 -0500

    Fixed Resume Viewer

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,19 +1,38 @@
-import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import { theme } from "antd";
 
 import { GlobalWorkerOptions } from "pdfjs-dist";
 import pdfWorker from "pdfjs-dist/build/pdf.worker.min?url";
 
-// 👇 Make versions match automatically
+const { useToken } = theme;
+
 GlobalWorkerOptions.workerSrc = pdfWorker;
 
 export default function ResumeViewer() {
-  return (
-    <div style={{display:"flex", flex:1, justifyContent:"center", alignContent:"center"}}>
-    <div style={{ height: "100vh", width:"76vw"}}>
-      <Viewer enableSmoothScroll defaultScale={0.8} fileUrl="/resume.pdf" />
-    </div>
-    </div>
+  const { token } = useToken();
 
+  // Pick your own rule for "dark"
+  const isDark = token.colorBgContainer === "#141414";
+  const viewerTheme = isDark ? "dark" : "light"; // or "auto"
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "76vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: token.colorBgContainer,
+      }}
+    >
+      <Viewer
+        theme={viewerTheme}
+        enableSmoothScroll
+        defaultScale={1.1}
+        fileUrl="/resume.pdf"
+      />
+    </div>
   );
 }
